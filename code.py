8dd0823aabf0e1de51fdb625cfc4a108147ff5ed
nikhil1235 <nikhilsingh1235@gmail.com>
2025-09-28 23:57:16 +0530

    new change

---
+++ code.py
@@ -0,0 +1,12 @@
+# Sorting an array of numbers in Python
+
+# Input: list of numbers
+numbers = [12, 4, 56, 17, 8, 99, 5]
+
+# Sort in ascending order
+numbers.sort()
+print("Ascending order:", numbers)
+
+# Sort in descending order
+numbers.sort(reverse=True)
+print("Descending order:", numbers)
